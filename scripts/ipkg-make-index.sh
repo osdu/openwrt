f403ce5c74ef9853a46e941b2a45b09016d485f4
osdu <858908467@qq.com>
2019-05-01 02:12:18 +0800

    Update scripts/ipkg-make-index.sh

--- scripts/ipkg-make-index.sh
+++ scripts/ipkg-make-index.sh
@@ -10,22 +10,41 @@ fi
 
 empty=1
 
-for pkg in `find $pkg_dir -name '*.ipk' | sort`; do
+for pkg in `find -L $pkg_dir -name '*.ipk' | sort`; do
 	empty=
 	name="${pkg##*/}"
 	name="${name%%_*}"
+
 	[[ "$name" = "kernel" ]] && continue
 	[[ "$name" = "libc" ]] && continue
+
 	echo "Generating index for package $pkg" >&2
 	file_size=$(stat -L -c%s $pkg)
-	sha256sum=$(mkhash sha256 $pkg)
+
+	# sha256sum=$(mkhash sha256 $pkg)
+	md5sum=$(md5sum $pkg | awk '{print $1}')
+
 	# Take pains to make variable value sed-safe
 	sed_safe_pkg=`echo $pkg | sed -e 's/^\.\///g' -e 's/\\//\\\\\\//g'`
-	tar -xzOf $pkg ./control.tar.gz | tar xzOf - ./control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
+
+	if tar -xzOf $pkg ./control.tar.gz >/dev/null 2>&1; then
+		tar -xzOf $pkg ./control.tar.gz | tar xzOf - ./control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
 Size: $file_size\\
-SHA256sum: $sha256sum\\
-Description:/"
+MD5Sum: $md5sum\\
+Description:/" |
+sed -e "s/^Description:[ ]*/Description: /" |
+sed -e '/^Compiler:/d; /^License:/d; /^LicenseFiles:/d; /^Source:/d; /^Maintainer:/d'
+	else
+		tar -xzOf $pkg control.tar.gz | tar xzOf - control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
+Size: $file_size\\
+MD5Sum: $md5sum\\
+Description:/" |
+sed -e "s/^Description:[ ]*/Description: /" |
+sed -e '/^Compiler:/d; /^License:/d; /^LicenseFiles:/d; /^Source:/d; /^Maintainer:/d'
+	fi
+
 	echo ""
 done
+
 [ -n "$empty" ] && echo
 exit 0
