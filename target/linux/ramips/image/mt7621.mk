cf12ab1f5b5b41dc2b0d0f85b369d06e199c8070
osdu <858908467@qq.com>
2019-08-18 02:15:14 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -203,7 +203,6 @@ define Device/XiaoYu-C1
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
   DEVICE_TITLE := XiaoYu C1
   DEVICE_PACKAGES := \
-	kmod-mt7603 kmod-mt76x2 \
 	kmod-ata-core kmod-ata-ahci kmod-ata-via-sata \
 	kmod-usb-core kmod-usb3 kmod-usb-ledtrig-usbport \
 	kmod-usb-storage kmod-usb-storage-extras
@@ -215,7 +214,6 @@ define Device/XiaoYu-C5
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
   DEVICE_TITLE := XiaoYu C5
   DEVICE_PACKAGES := \
-	kmod-mt7603 kmod-mt76x2 \
 	kmod-ata-core kmod-ata-ahci kmod-ata-via-sata \
 	kmod-usb-core kmod-usb3 kmod-usb-ledtrig-usbport \
 	kmod-usb-storage kmod-usb-storage-extras
